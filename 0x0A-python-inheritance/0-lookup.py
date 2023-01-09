4af0d7901b133206ee45abb359d36969f2e67338
Sommy <57346204+sommyvick@users.noreply.github.com>
2023-01-09 18:33:51 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,14 @@
-hfkffj
+#!/usr/bin/python3
+"""
+The lookup function container
+"""
+
+
+def lookup(obj):
+    """
+    Args:
+        obj: initial object
+        Returns: a list of available attributes and
+                 methods of an object
+    """
+    return dir(obj)

377e631c0ed4450bb21a309162cceff1c05bfb8a
Sommy <57346204+sommyvick@users.noreply.github.com>
2023-01-09 18:52:26 +0100

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """
-Contains the class BaseGeometry and subclass Rectangle
+101-add_attribute Module
 """
 
-Rectangle = __import__('9-rectangle').Rectangle
 
-
-class Square(Rectangle):
-    """A representation of a square class
-        Args:
-            size: size of square
+def add_attribute(obj, objname, value):
+    """add attribute to object
+    args:
+        obj: class object
+        objname: object name
+        value: value of attribute
     """
-    def __init__(self, size):
-        """instantiation of the square"""
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        """"returns the area of the square"""
-        return self.__size ** 2
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    setattr(obj, objname, value)

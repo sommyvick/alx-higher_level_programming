12de17490c4406ed3eb865aaf5ffcb36c26736b2
Sommy <57346204+sommyvick@users.noreply.github.com>
2023-01-09 18:39:10 +0100

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,12 @@
-dhjdkdh
+#!/usr/bin/python3
+"""
+This is a module container of the function 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class
+    (an empty class)
+    """
+    pass
